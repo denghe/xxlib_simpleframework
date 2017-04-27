11ef91e946e3d526798fb47ea7fcde4db1764a32
denghe <denghe@hotmail.com>
2017-04-28 00:57:18 +0800

    writing

--- projs/testuv_s/test2.cpp
+++ projs/testuv_s/test2.cpp
@@ -3,40 +3,79 @@
 #include <queue>
 int main()
 {
+	//{
+	//	xx::Stopwatch sw;
+	//	size_t counter = 0;
+	//	for (int j = 0; j < 999; j++)
+	//	{
+	//		std::queue<std::string> q;
+	//		for (int i = 0; i < 9999; i++)
+	//		{
+	//			q.push(std::to_string(i) + "12345678901234567890");
+	//		}
+	//		while (!q.empty())
+	//		{
+	//			counter += q.front().size();
+	//			q.pop();
+	//		}
+	//	}
+	//	std::cout << "elapsed ms = " << sw() << ", counter = " << counter << std::endl;
+	//}
+	//{
+	//	xx::MemPoolBase mpb;
+	//	xx::Stopwatch sw;
+	//	size_t counter = 0;
+	//	for (int j = 0; j < 999; j++)
+	//	{
+	//		xx::Queue_v<xx::String_v> q(mpb);
+	//		for (int i = 0; i < 9999; i++)
+	//		{
+	//			q->Emplace(mpb)->Append(i, "12345678901234567890");
+	//		}
+	//		while (!q->Empty())
+	//		{
+	//			counter += q->Peek()->dataLen;
+	//			q->Pop();
+	//		}
+	//	}
+	//	std::cout << "elapsed ms = " << sw() << ", counter = " << counter << std::endl;
+	//}
+
 	{
 		xx::Stopwatch sw;
 		size_t counter = 0;
-		for (int j = 0; j < 999; j++)
+		for (int j = 0; j < 9999; j++)
 		{
-			std::queue<std::string> q;
+			std::queue<int> q;
 			for (int i = 0; i < 9999; i++)
 			{
-				q.push(std::to_string(i) + "12345678901234567890");
+				q.push(i);
 			}
 			while (!q.empty())
 			{
-				counter += q.front().size();
+				counter += q.front();
 				q.pop();
 			}
 		}
 		std::cout << "elapsed ms = " << sw() << ", counter = " << counter << std::endl;
 	}
-
 	{
-		xx::MemPoolBase mpb;
 		xx::Stopwatch sw;
 		size_t counter = 0;
-		for (int j = 0; j < 999; j++)
 		{
-			xx::Queue_v<xx::String_v> q(mpb);
-			for (int i = 0; i < 9999; i++)
+			xx::MemPoolBase mpb;
+			for (int j = 0; j < 9999; j++)
 			{
-				q->Emplace(mpb)->Append(i, "12345678901234567890");
-			}
-			while (!q->Empty())
-			{
-				counter += q->Peek()->dataLen;
-				q->Pop();
+				xx::Queue_v<int> q(mpb);
+				for (int i = 0; i < 9999; i++)
+				{
+					q->Emplace(i);
+				}
+				while (!q->Empty())
+				{
+					counter += q->Peek();
+					q->Pop();
+				}
 			}
 		}
 		std::cout << "elapsed ms = " << sw() << ", counter = " << counter << std::endl;
