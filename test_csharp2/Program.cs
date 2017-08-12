418caa3fa5f531e216ea8fba998282f28e34f15c
denghe <denghe@hotmail.com>
2017-08-12 17:56:54 +0800

    add simple mj calc

--- test_csharp2/Program.cs
+++ test_csharp2/Program.cs
@@ -1,26 +1,125 @@
 ﻿using System.Diagnostics;
-using System.Threading.Tasks;
 
-public class Worker
+namespace 麻将
 {
-    public int i1, i2;
+    public enum 牌 : byte
+    {
+        一筒 = 1,
+        二筒,
+        三筒,
+        四筒,
+        五筒,
+        六筒,
+        七筒,
+        八筒,
+        九筒,
+        一条 = 11,
+        二条,
+        三条,
+        四条,
+        五条,
+        六条,
+        七条,
+        八条,
+        九条,
+        一万 = 21,
+        二万,
+        三万,
+        四万,
+        五万,
+        六万,
+        七万,
+        八万,
+        九万,
+        //红中 = 31,
+        //发财 = 32,  // 33
+        //白板 = 33,  // 35
+    }
 
-    void WorkCB()
+    public struct 牌张
     {
-        ++i1;
+        public 牌 牌;
+        public byte 张;
     }
-    void AfterWorkCB()
+
+    public class 手牌
     {
-        ++i2;
-    }
-    public void Execute()
-    {
-        for (int i = 0; i < 10000000; ++i)
+        public xx.List<牌> 牌s;
+
+        public xx.List<牌张> 牌张s = new xx.List<牌张>();
+
+        public void 填充牌张s()
         {
-            var t = new Task(() => { WorkCB(); });
-            t.Start();
-            Task.WaitAll(t);                        // 测试结果为约 50 万次 每秒
-            AfterWorkCB();
+            Debug.Assert(牌s != null && 牌s.dataLen > 0);
+            牌s.Sort();
+            牌张s.Clear();
+            var p = 牌s[0];
+            byte c = 1;
+            for (int i = 1; i < 牌s.dataLen; ++i)
+            {
+                if (牌s[i] == p)
+                {
+                    c++;
+                }
+                else
+                {
+                    牌张s.Add(new 牌张 { 牌 = p, 张 = c });
+                    p = 牌s[i];
+                    c = 1;
+                }
+            }
+            牌张s.Add(new 牌张 { 牌 = p, 张 = c });
+        }
+
+        // len:1, count:2/3 代表拿掉 对子/刻子.   len:3, count:1 代表拿掉顺子
+        public static xx.List<牌张> 拿掉牌(xx.List<牌张> 牌张s, int idx, int len, int count)
+        {
+            var ps = new xx.List<牌张>();
+            var buf = 牌张s.buf;
+            for (int i = 0; i < idx; ++i)
+            {
+                ps.Add(buf[i]);
+            }
+            for (int i = idx; i < idx + len; ++i)
+            {
+                var p = buf[i];
+                Debug.Assert(p.张 >= count);
+                if (p.张 > count)
+                {
+                    ps.Add(new 牌张 { 牌 = p.牌, 张 = (byte)(p.张 - count) });
+                }
+            }
+            for (int i = idx + len; i < 牌张s.dataLen; ++i)
+            {
+                ps.Add(buf[i]);
+            }
+            return ps;
+        }
+
+        public bool 简单判断是否能胡()
+        {
+            for (int i = 0; i < 牌张s.dataLen; ++i)
+            {
+                if (牌张s[i].张 >= 2 && 简单判断是否能胡(拿掉牌(牌张s, i, 1, 2))) return true;
+            }
+            return false;
+        }
+        public static bool 简单判断是否能胡(xx.List<牌张> 牌张s)
+        {
+            if (牌张s.dataLen == 0) return true;
+            // 拿掉刻
+            for (int i = 0; i < 牌张s.dataLen; ++i)
+            {
+                if (牌张s[i].张 >= 3 && 简单判断是否能胡(拿掉牌(牌张s, i, 1, 3))) return true;
+            }
+            // 拿掉顺
+            for (int i = 0; i < 牌张s.dataLen - 2; ++i)
+            {
+                if ((int)牌张s[i].牌 + 1 == (int)牌张s[i + 1].牌
+                    && (int)牌张s[i].牌 + 2 == (int)牌张s[i + 2].牌
+                    && 简单判断是否能胡(拿掉牌(牌张s, i, 3, 1))) return true;
+            }
+            return false;
         }
     }
 }
@@ -29,9 +128,17 @@ public static class Program
 {
     static void Main(string[] args)
     {
-        var w = new Worker();
-        var sw = Stopwatch.StartNew();
-        w.Execute();
-        System.Console.WriteLine(sw.ElapsedMilliseconds +  " i1 = " + w.i1 + " i2 = " + w.i2);
+        var sp = new 麻将.手牌
+        {
+            牌s = new xx.List<麻将.牌>(
+                麻将.牌.二万, 麻将.牌.二万, 麻将.牌.二万,
+                麻将.牌.一万, 麻将.牌.一万, 麻将.牌.一万,
+                麻将.牌.四万, 麻将.牌.四万,
+                麻将.牌.三万, 麻将.牌.三万, 麻将.牌.三万
+            )
+        };
+        sp.填充牌张s();
+        System.Console.WriteLine(sp.简单判断是否能胡());
     }
 }
+
