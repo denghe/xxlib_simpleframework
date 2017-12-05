4088bb35859393133519145ecd2f4d35cb675c4e
denghe <denghe@hotmail.com>
2017-12-05 23:37:28 +0800

    writing xxuvlib

--- xxuvlib/xxuvlib.cpp
+++ xxuvlib/xxuvlib.cpp
@@ -1,4 +1,5 @@
 ﻿#include "xxuvlib.h"
+#include <cstdlib>
 
 #ifdef _WIN32
 
@@ -20,4 +21,144 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 
 #endif
 
+static void* Alloc(size_t size)
+{
+	return std::malloc(size);
+}
 
+template<typename T>
+static T* Alloc()
+{
+	return (T*)Alloc(sizeof(T));
+}
+
+XXUVLIB_API void xxuv_free(void* p) noexcept
+{
+	std::free(p);
+}
+
+
+XXUVLIB_API const char* xxuv_strerror(int n) noexcept
+{
+	return uv_strerror(n);
+}
+XXUVLIB_API const char* xxuv_err_name(int n) noexcept
+{
+	return uv_err_name(n);
+}
+
+XXUVLIB_API void xxuv_close(uv_handle_t* handle, uv_close_cb close_cb) noexcept
+{
+	uv_close(handle, close_cb);
+}
+
+XXUVLIB_API void xxuv_close_(uv_handle_t* handle) noexcept
+{
+	uv_close(handle, [](uv_handle_t* handle)
+	{
+		xxuv_free(handle);
+	});
+}
+
+XXUVLIB_API uv_loop_t* xxuv_alloc_uv_loop_t() noexcept
+{
+	return ::Alloc<uv_loop_t>();
+}
+
+XXUVLIB_API uv_tcp_t* xxuv_alloc_uv_tcp_t() noexcept
+{
+	return ::Alloc<uv_tcp_t>();
+}
+
+XXUVLIB_API sockaddr_in* xxuv_alloc_sockaddr_in() noexcept
+{
+	return ::Alloc<sockaddr_in>();
+}
+
+
+XXUVLIB_API int xxuv_loop_init(uv_loop_t* p) noexcept
+{
+	return uv_loop_init(p);
+}
+
+XXUVLIB_API int xxuv_loop_close(uv_loop_t* p) noexcept
+{
+	return uv_loop_close(p);
+}
+
+
+
+XXUVLIB_API int xxuv_ip4_addr(const char* ip, int port, sockaddr_in* addr) noexcept
+{
+	return uv_ip4_addr(ip, port, addr);
+}
+
+
+
+XXUVLIB_API int xxuv_tcp_init(uv_loop_t* loop, uv_tcp_t* tcp) noexcept
+{
+	return uv_tcp_init(loop, tcp);
+}
+
+XXUVLIB_API int xxuv_tcp_bind(uv_tcp_t* tcp, const sockaddr* addr, unsigned int flags) noexcept
+{
+	return uv_tcp_bind(tcp, addr, flags);
+}
+XXUVLIB_API int xxuv_tcp_bind_(uv_tcp_t* tcp, const sockaddr* addr) noexcept
+{
+	return uv_tcp_bind(tcp, addr, 0);
+}
+
+
+XXUVLIB_API int xxuv_listen(uv_stream_t* tcp, int backlog, uv_connection_cb cb) noexcept
+{
+	return uv_listen(tcp, backlog, cb);
+}
+
+XXUVLIB_API int xxuv_accept(uv_stream_t* server, uv_stream_t* client) noexcept
+{
+	return uv_accept(server, client);
+}
+
+XXUVLIB_API int xxuv_read_start(uv_stream_t* client, uv_alloc_cb alloc_cb, uv_read_cb read_cb) noexcept
+{
+	return uv_read_start(client, alloc_cb, read_cb);
+}
+XXUVLIB_API int xxuv_read_start_(uv_stream_t* client, uv_read_cb read_cb) noexcept
+{
+	return uv_read_start(client, [](uv_handle_t*, size_t suggested_size, uv_buf_t* buf)
+	{
+		buf->base = (char*)Alloc(suggested_size);
+		buf->len = decltype(buf->len)(suggested_size);
+	}
+	, read_cb);
+}
+
+XXUVLIB_API int xxuv_write(uv_write_t* req, uv_stream_t* stream, const uv_buf_t bufs[], unsigned int nbufs, uv_write_cb cb)
+{
+	return uv_write(req, stream, bufs, nbufs, cb);
+}
+
+XXUVLIB_API int xxuv_write_(uv_stream_t* stream, char* buf, unsigned int len)
+{
+	struct write_req_t
+	{
+		uv_write_t req;
+		uv_buf_t buf;
+	};
+	auto req = Alloc<write_req_t>();
+	req->buf = uv_buf_init(buf, (uint32_t)len);
+	return uv_write((uv_write_t*)req, stream, &req->buf, 1, [](uv_write_t *req, int status)
+	{
+		if (status) fprintf(stderr, "Write error: %s\n", uv_strerror(status));
+		write_req_t *wr = (write_req_t*)req;
+		xxuv_free(wr->buf.base);
+		xxuv_free(wr);
+	});
+}
+
+
+XXUVLIB_API int xxuv_run(uv_loop_t* loop, uv_run_mode mode) noexcept
+{
+	return uv_run(loop, mode);
+}
--- xxuvlib/xxuvlib.h
+++ xxuvlib/xxuvlib.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <uv.h>
 
 #ifdef _WIN32
 	#include <SDKDDKVer.h>
@@ -17,9 +18,40 @@
 extern "C" {
 #endif
 
-	XXUVLIB_API void* xxuvNew();
+	XXUVLIB_API uv_loop_t* xxuv_alloc_uv_loop_t() noexcept;
+	XXUVLIB_API uv_tcp_t* xxuv_alloc_uv_tcp_t() noexcept;
+	XXUVLIB_API sockaddr_in* xxuv_alloc_sockaddr_in() noexcept;
+	XXUVLIB_API void xxuv_free(void* p) noexcept;
 
-	XXUVLIB_API void xxuvDelete();
+
+	XXUVLIB_API const char* xxuv_strerror(int n) noexcept;
+	XXUVLIB_API const char* xxuv_err_name(int n) noexcept;
+
+
+	XXUVLIB_API void xxuv_close(uv_handle_t* handle, uv_close_cb close_cb) noexcept;
+	XXUVLIB_API void xxuv_close_(uv_handle_t* handle) noexcept;
+
+	XXUVLIB_API int xxuv_loop_init(uv_loop_t* loop) noexcept;
+	XXUVLIB_API int xxuv_run(uv_loop_t* loop, uv_run_mode mode) noexcept;
+	XXUVLIB_API int xxuv_loop_close(uv_loop_t* loop) noexcept;
+
+	XXUVLIB_API int xxuv_ip4_addr(const char* ip, int port, sockaddr_in* addr) noexcept;
+
+	XXUVLIB_API int xxuv_tcp_init(uv_loop_t* loop, uv_tcp_t* tcp) noexcept;
+	XXUVLIB_API int xxuv_tcp_bind(uv_tcp_t* tcp, const sockaddr* addr, unsigned int flags) noexcept;
+	XXUVLIB_API int xxuv_tcp_bind_(uv_tcp_t* tcp, const sockaddr* addr) noexcept;
+	XXUVLIB_API int xxuv_listen(uv_stream_t* tcp, int backlog, uv_connection_cb cb) noexcept;
+	XXUVLIB_API int xxuv_accept(uv_stream_t* server, uv_stream_t* client) noexcept;
+	XXUVLIB_API int xxuv_read_start(uv_stream_t* client, uv_alloc_cb alloc_cb, uv_read_cb read_cb) noexcept;
+	XXUVLIB_API int xxuv_read_start_(uv_stream_t* client, uv_read_cb read_cb) noexcept;
+	XXUVLIB_API int xxuv_write(uv_write_t* req, uv_stream_t* stream, const uv_buf_t bufs[], unsigned int nbufs, uv_write_cb cb);
+	XXUVLIB_API int xxuv_write_(uv_stream_t* stream, char* buf, unsigned int len);
+
+	// todo
+	XXUVLIB_API int xxuv_is_readable(const uv_stream_t* handle);
+	XXUVLIB_API int xxuv_is_writable(const uv_stream_t* handle);
+	XXUVLIB_API size_t xxuv_stream_get_write_queue_size(const uv_stream_t* stream);
+	XXUVLIB_API int xxuv_try_write(uv_stream_t* handle, const uv_buf_t bufs[], unsigned int nbufs);
 
 #ifdef __cplusplus
 }
