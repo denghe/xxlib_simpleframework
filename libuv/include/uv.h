5103083b28ea8b2ea7402d772d3ebf1f191f49e1
denghe <denghe@hotmail.com>
2018-02-10 22:05:13 +0800

    upgrade libuv lib to latest

--- libuv/include/uv.h
+++ libuv/include/uv.h
@@ -1,4 +1,4 @@
-﻿/* Copyright Joyent, Inc. and other Node contributors. All rights reserved.
+/* Copyright Joyent, Inc. and other Node contributors. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
