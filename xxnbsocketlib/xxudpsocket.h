466ab02151b11083bf5901287d07ba7a08b0fb4a
denghe <denghe@hotmail.com>
2018-03-13 18:50:49 +0800

    writing( need write lua bind for XxUdpSocket )

--- xxnbsocketlib/xxudpsocket.h
+++ xxnbsocketlib/xxudpsocket.h
@@ -21,29 +21,31 @@ struct XxUdpSocket
 		Disconnecting,								// 执行 Disconnect( 延迟断开时长 ) 之后
 	};
 
-	uv_loop_t*					uv = nullptr;
-	uv_udp_t*					udp = nullptr;
-	ikcpcb*						kcp = nullptr;
-	Guid guid;
+	uv_loop_t*						uv = nullptr;
+	uv_udp_t*						udp = nullptr;
+	ikcpcb*							kcp = nullptr;
+	Guid							guid;
+	int								interval = 33;	// 在 connect 时可以改. 默认对应 30 fps 的间隔时间
 
-	sockaddr_in6				addr;				// ipv4/6 公用结构( ipv4 时硬转为 sockaddr_in )
-	int							ticks = 0;			// 当前状态持续 ticks 计数 ( Disconnecting 时例外, 该值为负, 当变到 0 时, 执行 Close )
-	States						state = States::Disconnected;
+	sockaddr_in6					addr;			// ipv4/6 公用结构( ipv4 时硬转为 sockaddr_in )
+	int								ticks = 0;		// 当前状态持续 ticks 计数 ( Disconnecting 时例外, 该值为负, 当变到 0 时, 执行 Close )
+	States							state = States::Disconnected;
 
-	std::deque<XxBuf>			sendBufs;			// 未及时发走的数据将堆积在此
-	std::deque<XxBuf>			recvBufs;			// 收到的"包"数据将堆积在此
+	int								readLen = 0;	// 接收缓冲区已存在的数据长度
+	std::array<char, 131075>		readBuf;		// 接收缓冲区( 至少能含 1 个 64k 完整包 + 1 段上次处理剩下的数据 即 64k * 2 + 1.5 个包头 3 字节 )
 
-	int							readLen = 0;		// 接收缓冲区已存在的数据长度
-	std::array<char, 131075>	readBuf;			// 接收缓冲区( 至少能含 1 个 64k 完整包 + 1 段上次处理剩下的数据 即 64k * 2 + 1.5 个包头 3 字节 )
+	std::deque<XxBuf>				recvBufs;		// 收到的"包"数据将堆积在此
 
 	XxUdpSocket()
 	{
-		uv = uv_default_loop();
+		// todo: 创建独立 uv 结构( 先不做 )
+		uv = uv_default_loop();		
 		addr.sin6_family = 0;						// 用这个来做是否有设置过 addr 的标记
 	}
 
 	~XxUdpSocket()
 	{
+		Close(0);
 		// todo: 析构 uv( 先不做 )
 	}
 
@@ -109,7 +111,7 @@ struct XxUdpSocket
 	// 并非连接目标地址服务器. 只是初始化 socket kcp 和参数些. udp 不需要连接.
 	// 返回非 0 表示出错.
 	inline int Connect(int sndwnd = 128, int rcvwnd = 128
-		, int nodelay = 1, int interval = 10, int resend = 2, int nc = 1, int minrto = 100)
+		, int nodelay = 1, int interval = 33, int resend = 2, int nc = 1, int minrto = 100)
 	{
 		if (state != States::Disconnected) return -2;	// wrong state
 		if (!addr.sin6_family) return -3;				// not set address ?
@@ -121,10 +123,9 @@ struct XxUdpSocket
 #else
 		uuid_generate(reinterpret_cast<unsigned char *>(&guid));
 #endif
-		
-		// todo: kcp_create, ...
+
 		udp = (uv_udp_t*)Alloc(sizeof(uv_udp_t), this);
-		if (udp == nullptr) return -5;
+		if (!udp) return -5;
 
 		if (int r = uv_udp_init(uv, udp))
 		{
@@ -137,10 +138,42 @@ struct XxUdpSocket
 		{
 			Close((uv_handle_t*)udp);
 			udp = nullptr;
-			throw r;
+			return r;
 		}
 
-		// todo: kcp_create
+		kcp = ikcp_create(&guid, this, nullptr);
+		if (!kcp)
+		{
+			Close((uv_handle_t*)udp);
+			udp = nullptr;
+			return -6;
+		}
+
+		ikcp_wndsize(kcp, sndwnd, rcvwnd);
+		ikcp_nodelay(kcp, nodelay, interval, resend, nc);
+		this->interval = interval;
+
+		ikcp_setoutput(kcp, [](const char *inBuf, int len, ikcpcb *kcp)->int
+		{
+			auto self = (XxUdpSocket*)kcp->user;
+			struct uv_udp_send_t_ex
+			{
+				uv_udp_send_t req;
+				uv_buf_t buf;
+			};
+			auto req = (uv_udp_send_t_ex*)Alloc(sizeof(uv_udp_send_t_ex));
+			auto buf = (char*)Alloc(len);
+			memcpy(buf, inBuf, len);
+			req->buf = uv_buf_init(buf, (uint32_t)len);
+			return uv_udp_send((uv_udp_send_t*)req, self->udp, &req->buf, 1, (sockaddr*)&self->addr, [](uv_udp_send_t* req, int status)
+			{
+				//if (status) fprintf(stderr, "Write error: %s\n", uv_strerror(status));
+				auto *wr = (uv_udp_send_t_ex*)req;
+				Free(wr->buf.base);
+				Free(wr);
+			});
+
+		});
 
 		state = States::Connected;
 		ticks = 0;
@@ -167,119 +200,63 @@ struct XxUdpSocket
 	inline int Update(int const& sec = 0, int const& usec = 0)
 	{
 		++ticks;
+		uv_run(uv, uv_run_mode::UV_RUN_NOWAIT);
 		switch (state)
 		{
-		case States::Disconnected:
-		{
-			return 0;
-		}
 		case States::Connecting:
 		{
-			return -1;								// invalid state
-		}
-		case States::Connected:
-		{
-			// todo: call uv run nowait
-
-		//	// 判断 socket 是否处于 待接收数据 状态
-		//	int r = SockWaitReadOrWritable(sock, true, sec, usec);
-		//	if (r < 0) return Close(r);
-		//	else if (r > 0)
-		//	{
-		//	LabRetry:
-		//		r = ::recv(sock, readBuf.data() + readLen, (int)readBuf.size() - readLen, 0);
-		//		if (r < 0)
-		//		{
-		//			r = SockGetErrNo();
-		//			if (r == EINTR) goto LabRetry;
-		//			if (r != EAGAIN && r != EWOULDBLOCK) return Close(-1);
-		//		}
-		//		else if (r == 0) return Close(-2);
-		//		else // r > 0
-		//		{
-		//			readLen += r;
-
-		//			auto buf = (uint8_t*)readBuf.data();			// 无符号方便位操作读出头
-		//			int offset = 0;									// 游标
-
-		//			while (readLen - offset >= 3)					// 确保 3字节 包头长度
-		//			{
-		//				// 读出长度信息( 首字节 pkg type id 用不到, 直接跳过 )
-		//				auto dataLen = buf[offset + 1] + (buf[offset + 2] << 8);
-		//				if (!dataLen) return Close(-3);				// 异常: 读不到长度, 关闭连接
-		//				dataLen += 3;								// 将包头的长度纳入
-		//				if (offset + dataLen > readLen) break;		// 确保数据长
-
-		//				// 将数据弄到 recvBufs( 含包头, 以便上层代码继续解析 )
-		//				recvBufs.emplace_back((char*)buf + offset, dataLen);
-		//				offset += dataLen;
-		//			}
-
-		//			// 剩余数据移到首部后退出
-		//			readLen -= offset;
-		//			if (readLen)
-		//			{
-		//				memmove(buf, buf + offset, readLen);
-		//			}
-		//		}
-		//	}
-
-		//	// 判断是否有待发数据, 持续发. 有数据收则收到队列. 
-		//	if (!sendBufs.empty())
-		//	{
-		//		auto& front = sendBufs.front();
-		//		auto len = front.dataLen - front.offset;
-		//		auto r = SockSend(sock, front.buf + front.offset, len);
-		//		if (r.first) return Close(-2);
-		//		if (r.second < (int)len)
-		//		{
-		//			front.offset += r.second;
-		//		}
-		//		else
-		//		{
-		//			sendBufs.pop_front();
-		//		}
-		//	}
-			return 0;
+			return -1;								// udp 不存在这个状态
 		}
 		case States::Disconnecting:
 		{
 			if (!ticks) Close(0);
-			return 0;
 		}
-		default:
-			return -1;								// unknown state
 		}
+		ikcp_update(kcp, interval * ticks);
+		while (true)
+		{
+			int r = ikcp_recv(kcp, readBuf.data() + readLen, (int)readBuf.size() - readLen);
+			if (r < 0) break;
+			else if (r == 0) return Close(-2);
+			else // r > 0
+			{
+				readLen += r;
 
+				auto buf = (uint8_t*)readBuf.data();			// 无符号方便位操作读出头
+				int offset = 0;									// 游标
+
+				while (readLen - offset >= 3)					// 确保 3字节 包头长度
+				{
+					// 读出长度信息( 首字节 pkg type id 用不到, 直接跳过 )
+					auto dataLen = buf[offset + 1] + (buf[offset + 2] << 8);
+					if (!dataLen) return Close(-3);				// 异常: 读不到长度, 关闭连接
+					dataLen += 3;								// 将包头的长度纳入
+					if (offset + dataLen > readLen) break;		// 确保数据长
+
+																// 将数据弄到 recvBufs( 含包头, 以便上层代码继续解析 )
+					recvBufs.emplace_back((char*)buf + offset, dataLen);
+					offset += dataLen;
+				}
+
+				// 剩余数据移到首部后退出
+				readLen -= offset;
+				if (readLen)
+				{
+					memmove(buf, buf + offset, readLen);
+				}
+			}
+		}
 		return 0;
 	}
 
 
-	// 返回负数表示出错. 0 表示成功放入待发送队列或无数据可发. > 0 表示已立刻发送成功的长度. 
-	// 如果有剩下部分, 会放入待发送队列, 在下次 Update 时继续发
+	// 返回非 0 表示出错
 	inline int Send(char const* const& buf, int const& dataLen)
 	{
 		if (dataLen <= 0) return -1;
+		if (state != States::Connected) return -1;
 
-		//// 判断当前是否存在待发数据. 如果有就追加到后面 并返回 0
-		//if (!sendBufs.empty())
-		//{
-		//	sendBufs.emplace_back(buf, dataLen);
-		//	return 0;
-		//}
-
-		//// 直接发. 如果出错则关闭 socket, 改 state 并返回错误码.
-		//auto r = SockSend(sock, buf, dataLen);
-		//if (r.first) return Close(-2);
-
-		//// 将没发完的数据追加到待发
-		//if (dataLen > r.second)
-		//{
-		//	sendBufs.emplace_back(buf + r.second, dataLen - r.second);
-		//}
-		//return r.second;
-
-		return 0;
+		return ikcp_send(kcp, buf, dataLen);
 	}
 
 	// Send 之 BB 版
@@ -293,15 +270,13 @@ private:
 	// 关闭连接, 清状态 及 收发包, 返回传入的错误以简化一些代码书写
 	inline int Close(int e)
 	{
-		// todo
-		//SockClose(sock);
-		//sock = -1;
-
+		if (state == States::Disconnected) return e;
 		state = States::Disconnected;
 		ticks = 0;
-		readLen = 0;
-		sendBufs.clear();	// todo: 可能不再需要这两个容器
-		recvBufs.clear();
+		Close((uv_handle_t*)udp);
+		udp = nullptr;
+		ikcp_release(kcp);
+		kcp = nullptr;
 		return e;
 	}
 };
