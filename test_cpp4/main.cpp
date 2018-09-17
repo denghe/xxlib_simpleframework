9e7dabe858e31ad5f4f915ff1820d6ecd97e366a
denghe <denghe@hotmail.com>
2018-09-18 02:47:43 +0800

    testing lua

--- test_cpp4/main.cpp
+++ test_cpp4/main.cpp
@@ -1,6 +1,46 @@
 ﻿#include "xx.h"
+#include "lua.hpp"
+
+int Lmain(lua_State *L)
+{
+	luaL_openlibs(L);
+
+	auto s = R"(
+local t = {}
+for j = 1, 10, 1 do
+	for i = 1, 9999999, 1 do
+		t[i] = i
+	end
+	for i = 1, 9999999, 1 do
+		t[i] = nil
+	end
+end
+)";
+	if (int r = luaL_loadstring(L, s))
+	{
+		std::cout << "r = " << r << ", errmsg = " << lua_tostring(L, -1) << std::endl;
+		lua_pop(L, 1);
+		return 0;
+	}
+
+	lua_call(L, 0, LUA_MULTRET);
+
+	return 0;
+}
 
 int main()
 {
+	auto L = luaL_newstate();
+	if (!L) return -1;
 
+	lua_pushcclosure(L, &Lmain, 0);
+
+	if (int r = lua_pcall(L, 0, LUA_MULTRET, 0))
+	{
+		std::cout << "r = " << r << ", errmsg = " << lua_tostring(L, -1) << std::endl;
+		lua_pop(L, 1);
+		return r;
+	}
+
+	return 0;
 }
