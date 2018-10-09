d23c0ffe6d8be3532d93ecd1d6a0751d0dfd79ec
FJSDS <821431199@qq.com>
2018-10-09 18:41:34 +0800

    add XXList.ToGenericList and impl hello

--- web/hello.aspx.cs
+++ web/hello.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -17,12 +18,26 @@ namespace web
         protected void Page_Load(object sender, EventArgs e)
         {
             // todo: session ckeck
+            Global.MySqlExecute(fs =>
+            {
+                var sb = new StringBuilder();
+                var ids = fs.Manager_SelectIds(999, "", "order by `id` ASC");
+                //gridManagers.RecordCount = Math.Min(999, ids.dataLen);
+               
+                var pageIds = ids.ToGenericList().GetRange(GridView1.PageIndex * GridView1.PageSize, System.Math.Min(GridView1.PageSize, ids.dataLen));
+                var tmp = new xx.List<int>();
+                tmp.AddRange(pageIds);
+                var ms = fs.Manager_SelectByIds<WEB.Manager>(tmp, "order by `id` ASC");
+                GridView1.DataSource = ms.ToGenericList();
+            });
+            GridView1.DataBind();
         }
 
-        protected void btnHello_Click(object sender, EventArgs e)
-        {
-            Alert.Show("你好 FineUI！", MessageBoxIcon.Warning);
-        }
+
+        //protected void btnHello_Click(object sender, EventArgs e)
+        //{
+        //    Alert.Show("你好 FineUI！", MessageBoxIcon.Warning);
+        //}
 
     }
 }
