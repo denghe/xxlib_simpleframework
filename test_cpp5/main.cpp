cab0ea214441dba76aae0a2eff03275484fc147f
denghe <denghe@hotmail.com>
2018-10-27 12:24:44 +0800

    add some test

--- test_cpp5/main.cpp
+++ test_cpp5/main.cpp
@@ -1,76 +1,39 @@
 ﻿#pragma execution_character_set("utf-8")
-//#include "../pkg/PKG_class.h"
-
-// todo: DictEx Update 函数还需要进一步测试. 看上去有老 HashCode 没被更新的问题
-
-#include "xx_uv.h"
-
-xx::MemPool mp;
-
-template<typename...Args>
-void Cout(Args const&...args)
-{
-	xx::String s(&mp);
-	s.Append(args...);
-	std::cout << s;
-}
+#include "xx.h"
 
 struct Foo
 {
-	int v1, v2;
+	std::string s1;
+	Foo()
+	{
+		std::cout << "create" << std::endl;
+	}
+	Foo(Foo const& o)
+		: s1(o.s1)
+	{
+		std::cout << "copy" << std::endl;
+	}
+	Foo(Foo&& o)
+		: s1(std::move(o.s1))
+	{
+		std::cout << "move" << std::endl;
+	}
+	~Foo()
+	{
+		std::cout << "release" << std::endl;
+	}
 };
 
-int main2()
-{
-	xx::DictEx<Foo, decltype(Foo::v1), decltype(Foo::v2)> de(&mp);
-
-	{
-		Foo f{ 1, 2 };
-
-		auto r = de.Add(f, f.v1, f.v2);
-		Cout(r.success, ", ", r.index, '\n');
-
-		r = de.Add(f, f.v1, f.v2);
-		Cout(r.success, ", ", r.index, '\n');	// false
-	}
-
-	{
-		Foo f{ 2, 3 };
-
-		auto r = de.Add(f, f.v1, f.v2);
-		Cout(r.success, ", ", r.index, '\n');
-
-		f.v1 = 3;
-		r = de.Add(f, f.v1, f.v2);
-		Cout(r.success, ", ", r.index, '\n');	// false
-	}
-
-	de.Add(Foo{}, 3, 4);
-	de.Add(Foo{}, 4, 5);
-	de.Add(Foo{}, 5, 6);
-	de.Add(Foo{}, 6, 7);
-
-	Cout("find 0/3, 1/4 : ", de.Find<0>(3), " ", de.Find<1>(4), "\n");
-
-	de.Remove<1>(4);
-	assert(de.Find<0>(3) == -1);
-	int idx = de.Find<0>(4);
-	de.RemoveAt(idx);
-
-	bool ok = de.Update<0>(1, 3);
-	assert(ok);
-
-	for (decltype(auto) iv : de)
-	{
-		Cout("k0: ", de.KeyAt<0>(iv.index), ", k1: ", de.KeyAt<1>(iv.index), ", foo = ", iv.value.v1, ", ", iv.value.v2, "\n");
-	}
-
-	return 0;
-}
+std::function<void()> func;
+//kapala::fixed_function<void()> func;
 
 int main()
 {
-	main2();
+	std::cout << "1" << std::endl;
+	func = [f = std::move(Foo())]{};
+	std::cout << "2" << std::endl;
+	func = nullptr;
+	std::cout << "3" << std::endl;
+
 	return 0;
 }
-
